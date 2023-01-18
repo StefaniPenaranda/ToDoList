9a7dabf9c3dd29b9d779d608a9158b088befd5ab
@StefaniPenaranda <pstefanipm@gmail.com>
2023-01-18 14:44:16 -0500

    evento onclick en TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <input 
       className="TodoSearch" 
       placeholder="Search..."
-      onChange={} 
+      onChange={onSearchValueChange} 
     />
   );
 }
