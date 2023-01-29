ab1057178df233d53c80f0ae0aec46de56bce4e6
@StefaniPenaranda <pstefanipm@gmail.com>
2023-01-28 21:02:03 -0500

    condicionamiento de hoock de loging , error

--- src/page/App.js
+++ src/page/App.js
@@ -12,20 +12,73 @@ import '../styles/pages/App.css';
 //   { text: 'Llorar con la llorona', completed: false },
 //   { text: 'LALALALAA', completed: false },
 // ];
+//Hook para reemplazr el React.usatate
+//itemName = nombre del elemeto con el que trabjaremos, el elemeto que recogeremos y salvaremos dentro del  localStorage
+function useLocalStorage(itemName, initialValue){
+  //llamamos a un react Hoock oficial,
+  const [error, setError ] = React.useState(false)
+  const [loading, setLoading ] = React.useState(true) 
+  const [item, setItem] = React.useState(initialValue); 
+
+  React.useEffect(()=>{
+    // Simulamos un segundo de delay de carga
+    setTimeout(()=>{
+      try {
+        // llamando al localStorage traer el elemento que nos envian como argumneto en la funcion useLocalStorage()
+        const localStorageItem = localStorage.getItem(itemName);         
+        let parsedItem;// para los usuarios nuevos que nunca han tenido una tarea
+        
+        if (!localStorageItem) {
+          // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          // Si existen TODOs en el localStorage los regresamos como nuestros todos
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+      }
+      catch(error){
+        setError(error)
+      }finally {
+        // También podemos utilizar la última parte del try/cath (finally) para terminar la carga
+        setLoading(false);
+      }
+   
+    },1000)
+})
+
+  
+
+  //funciona para 
+  const saveItem = (newItem)=>{
+    // Manejamos la tarea dentro de un try/catch por si ocurre algún error
+    try{
+      const stringConvertItem = JSON.stringify(newItem);//convirtiendo los Item a string
+      localStorage.setItem(itemName,stringConvertItem);
+      // llamamos localStorage(donde queremos guardar la info, enviamos el strign con la nueva actualizacion de todos
+      // )
+      setItem(newItem);
+    } catch(error){
+      setError(error)
+    }
+
+  };
+  // Para tener un mejor control de los datos retornados, podemos regresarlos dentro de un objeto
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+}
+
 
 function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;// para los usuarios nuevos que nunca han tenido una tarea
-  if (!localStorageTodos) {
-    // Si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos uno con un array vacío
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    // Si existen TODOs en el localStorage los regresamos como nuestros todos
-    parsedTodos = JSON.parse(localStorageTodos);
-  }
+  // nos devolvera los item el elemento que hayamos guardado en localStorage;
+  const {item: todos, saveItem: saveTodos, loading , error} = useLocalStorage('TODOS_V1', []);
 
-  const [todos, setTodos] = React.useState(parsedTodos); 
   // creandonnuestro estado
   const [searchValue, setSearchValue] = React.useState('');
   //contamos cuantos todos tenemos completados y cuantontenemos entotoal
@@ -46,24 +99,19 @@ function App() {
     //   console.log(searchText) 
     //   return todoText.includes(searchText);
     // })
-    searchTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
+    searchTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
   }
-//funciona para 
-const safeTodos = (newTodos)=>{
-    const stringConvertTodos = JSON.stringify(setTodos);//convirtiendo los todos a string
-    localStorage.setItem('TODO_V1',stringConvertTodos);
-     // llamamos localStorage(donde queremos guardar la info, enviamos el strign con la nueva actualizacion de todos
-     // )
-     setTodos(newTodos);
-  };
-
 
   // funcion para marcra comocompletados 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    todos[todoIndex].completed = true;
-    setTodos(newTodos)
+    newTodos[todoIndex].completed = true;
+    saveTodos(newTodos)
     }
 
     const DeletTodo = (text) => {
@@ -71,8 +119,14 @@ const safeTodos = (newTodos)=>{
       const newTodos = [...todos];
       //splice nos permite sacra una parte ( donde empezamos,cuantos sacamos)
       newTodos.splice(todoIndex,1);
-      setTodos(newTodos)
+      saveTodos(newTodos)
       }
+//  console.log('antes')
+//   React.useEffect(()=>{
+//     console.log('dentro de use')
+//   }, [totalTodos])
+// console.log('despues')
+
   return (
     <React.Fragment>
       <TodoSearch 
@@ -87,6 +141,9 @@ const safeTodos = (newTodos)=>{
       
 
       <TodoList>
+        {loading && <p>We are loading</p>}
+        {error && <p>ahhh mistake, run</p>}
+        {(!loading && !searchTodos.length) && <p> Create your first Todo</p>}
         {searchTodos.map(todo => (
           <TodoItem
             key={todo.text}
